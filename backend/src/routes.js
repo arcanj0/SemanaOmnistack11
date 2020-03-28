a4b121303c57dc904f7976e317b5db065cc9fa36
Rafael de Castro Almeida <rdecastroalmeid1@ncstudents.niagaracollege.ca>
2020-03-27 23:21:53 -0400

    some validations to routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { celebrate, Segments, Joi } = require("celebrate");
 
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
@@ -19,16 +20,66 @@ Route Params: used to identify resources (/users/:id)
 Request Body: it is the body of the request, used to get the params passed by url. It uses request.params
 */
 
+//Create Session/Login
 routes.post("/sessions", SessionController.create);
 
+/* ONGS */
+//List ongs
 routes.get("/ongs", OngController.index);
-routes.post("/ongs", OngController.create);
 
-routes.get("/profile", ProfileController.index);
+//Create ONG
+routes.post(
+  "/ongs",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string()
+        .required()
+        .email(),
+      whatsapp: Joi.number()
+        .required()
+        .min(10)
+        .max(11),
+      city: Joi.string().required(),
+      uf: Joi.string()
+        .required()
+        .length(2)
+    })
+  }),
+  OngController.create
+);
 
-routes.get("/incidents", IncidentController.index);
+//List Profile
+routes.get(
+  "/profile",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+  ProfileController.index
+);
+
+/* INCIDENTS */
+//List incidents
+routes.get(
+  "/incidents",
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number()
+    })
+  }),
+  IncidentController.index
+);
 routes.post("/incidents", IncidentController.create);
-routes.delete("/incidents/:id", IncidentController.delete);
+
+routes.delete(
+  "/incidents/:id",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({ id: Joi.number().required() })
+  }),
+  IncidentController.delete
+);
 
 //make it available to other source files to access/import
 module.exports = routes;
